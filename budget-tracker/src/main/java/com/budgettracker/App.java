17760787cb8ae95eb5752c410aa4bed6d774122f
Andrijana Canic <andrijanacanic2000@gmail.com>
2025-08-12 22:48:14 +0200

    Read JSON file and print content

--- budget-tracker/src/main/java/com/budgettracker/App.java
+++ budget-tracker/src/main/java/com/budgettracker/App.java
@@ -1,13 +1,17 @@
 package com.budgettracker;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        Path json = Path.of("src/main/resources/data.json");
+        String content = Files.readString(json);
+        System.out.println("=== data.json ===");
+        System.out.println(content);
+        System.out.println("=================");
+        System.out.println("File length: " + content.length() + " characters");
+
     }
 }
