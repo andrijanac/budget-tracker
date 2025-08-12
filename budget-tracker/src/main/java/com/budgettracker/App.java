0bb9f75fe59b1b4ba65e75f0becb9ce2fafcad0d
Andrijana Canic <andrijanacanic2000@gmail.com>
2025-08-12 21:41:00 +0200

    Initial Maven project setup

---
+++ budget-tracker/src/main/java/com/budgettracker/App.java
@@ -0,0 +1,13 @@
+package com.budgettracker;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
