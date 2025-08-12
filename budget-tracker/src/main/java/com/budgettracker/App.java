a571eb45cd8b91863c82a1b2d8243618b2a6d81b
Andrijana Canic <andrijanacanic2000@gmail.com>
2025-08-13 00:10:14 +0200

    Used GSON to read transactions from data.json and display them in the console

--- budget-tracker/src/main/java/com/budgettracker/App.java
+++ budget-tracker/src/main/java/com/budgettracker/App.java
@@ -2,16 +2,25 @@ package com.budgettracker;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 public class App {
     public static void main(String[] args) throws Exception {
 
-        Path json = Path.of("src/main/resources/data.json");
-        String content = Files.readString(json);
-        System.out.println("=== data.json ===");
-        System.out.println(content);
-        System.out.println("=================");
-        System.out.println("File length: " + content.length() + " characters");
+        Path jsonPath = Path.of("src/main/resources/data.json");
+        String content = Files.readString(jsonPath);
 
+        Gson gson = new Gson();
+        List<Transaction> transactions = gson.fromJson(
+            content,
+            new TypeToken<List<Transaction>>() {}.getType()
+        );
+
+        System.out.println("Total transactions: " + transactions.size());
+        for (Transaction t : transactions) {
+            System.out.println(t);
+        }
     }
 }
